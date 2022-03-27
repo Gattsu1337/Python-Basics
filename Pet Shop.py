75a01b55ee4e31a71e487c6cdb555ae93b53dd0b
Dimitar Velikov <102250347+Gattsu1337@users.noreply.github.com>
2022-03-27 10:19:30 +0300

    Add files via upload

---
+++ Pet Shop.py
@@ -0,0 +1,6 @@
+dog_food = int(input())
+cat_food = int(input())
+price_of_dog_food = dog_food * 2.50
+price_of_cat_food = cat_food * 4
+final_price = price_of_cat_food + price_of_dog_food
+print(f"The final price is {final_price} lv.")
