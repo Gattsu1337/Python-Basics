7d8f4a79b4df37a2ea83823b21d25a45818a9957
Dimitar Velikov <102250347+Gattsu1337@users.noreply.github.com>
2022-04-10 00:21:59 +0300

    Add files via upload

---
+++ cinema_ticket.py
@@ -0,0 +1,24 @@
+day = input()
+ticket_price = 0
+if day == "Monday":
+    ticket_price = 12
+    print(ticket_price)
+elif day == "Tuesday":
+    ticket_price = 12
+    print(ticket_price)
+elif day == "Wednesday":
+    ticket_price = 14
+    print(ticket_price)
+elif day == "Thursday":
+    ticket_price = 14
+    print(ticket_price)
+elif day == "Friday":
+    ticket_price = 12
+    print(ticket_price)
+elif day == "Saturday":
+    ticket_price = 16
+    print(ticket_price)
+elif day == "Sunday":
+    ticket_price = 16
+    print(ticket_price)
+
